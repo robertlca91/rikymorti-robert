9952cac01f3a719c3aa82078b0a4779536431cc9
robert <robert_lca91@hotmail.com>
2022-11-06 20:36:17 -0500

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Locations from './components/Locations'
 
 import './App.css'
@@ -9,10 +10,11 @@ function App() {
     <div className="App">
       <Locations/>
       <footer>
+        <img className="logo" src="https://o.remove.bg/downloads/9864614c-be37-44ce-99b6-503ce13e6bad/images-removebg-preview.png" alt=""/>
         <h3>DEVELOPERS</h3>
         <p>Paul Ruiz</p>
         <p>Miguel Martinez</p>
-        <p>Roberto Centeno</p>
+        <p>Roberto Centeno </p>
       </footer>
     </div>
   )
